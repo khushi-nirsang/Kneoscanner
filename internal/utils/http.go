083e806da4bbd5551daaec6f28c6c66655212c51
Khushi Singh <134994971+khushi-nirsang@users.noreply.github.com>
2026-06-03 14:40:52 +0530

    Update http.go

--- internal/utils/http.go
+++ internal/utils/http.go
@@ -1,1 +1,17 @@
+package utils
 
+import (
+	"net/http"
+	"time"
+)
+
+func NewHTTPClient() *http.Client {
+	return &http.Client{
+		Timeout: 15 * time.Second,
+	}
+}
+
+func Get(url string) (*http.Response, error) {
+	client := NewHTTPClient()
+	return client.Get(url)
+}
